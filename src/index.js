213d20e0aae46bdaaf09e36479702d1bca8c456f
TatianeMarinho <devtatianemarinho@gmail.com>
2023-10-11 01:50:55 -0400

    endpoint GET /talker/:id

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
-
-
 app.use('/talker', talkerRouter);
 
 // não remova esse endpoint, e para o avaliador funcionar
--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -11,4 +11,21 @@ router.get('/', async (_req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const talker = await readFile();
+    const result = talker.find((element) => element.id === Number(id));
+    if (result) {
+      return res.status(200).json(result);
+    } 
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.sqlMessage });
+  }
+});
+
 module.exports = router;
